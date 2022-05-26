7902bce889fbc32651bc22ae3dbcbfd64212b913
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-05-26 13:20:22 +0200

    Spring Hibernate. Dzień 1. Praca z encjami. Zadanie 1

---
+++ src/main/java/com/piotrgrochowiecki/bookstore/model/Book.java
@@ -0,0 +1,22 @@
+package com.piotrgrochowiecki.bookstore.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Getter @Setter @ToString
+@Entity(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private int rating;
+    private String description;
+
+}
