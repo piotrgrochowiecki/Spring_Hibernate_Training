f78eed5ee153c97d4a97ba7d8f80ac54eb29ebf6
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-06-04 01:09:02 +0200

    Spring Hibernate. Dzień 3. Wprowadzenie do walidacji. Zadanie 5

--- src/main/java/com/piotrgrochowiecki/bookstore/model/Publisher.java
+++ src/main/java/com/piotrgrochowiecki/bookstore/model/Publisher.java
@@ -3,8 +3,11 @@ package com.piotrgrochowiecki.bookstore.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.pl.NIP;
+import org.hibernate.validator.constraints.pl.REGON;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +18,12 @@ public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @NIP
+    private String nip;
+    @REGON
+    private String regon;
 
     @OneToMany(mappedBy = "publisher", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @ToString.Exclude
