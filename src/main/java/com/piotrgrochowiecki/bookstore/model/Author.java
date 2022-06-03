914da629dda7455049cccd092f0369425bb92235
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-06-04 01:08:05 +0200

    Spring Hibernate. Dzień 3. Wprowadzenie do walidacji. Zadanie 4

--- src/main/java/com/piotrgrochowiecki/bookstore/model/Author.java
+++ src/main/java/com/piotrgrochowiecki/bookstore/model/Author.java
@@ -3,8 +3,11 @@ package com.piotrgrochowiecki.bookstore.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.pl.PESEL;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,8 +18,14 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @PESEL
+    private String pesel;
+    @Email
+    private String email;
 
     @Transient
     public String getFullName() {
