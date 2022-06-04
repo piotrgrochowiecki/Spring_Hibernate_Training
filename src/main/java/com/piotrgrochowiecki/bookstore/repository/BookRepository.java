ee4b51658896738fcf4544306fc28bbbd1e62f7c
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-06-04 19:00:14 +0200

    Spring Hibernate. Dzień 3. Tworzenie zapytań. Zadanie 3

--- src/main/java/com/piotrgrochowiecki/bookstore/repository/BookRepository.java
+++ src/main/java/com/piotrgrochowiecki/bookstore/repository/BookRepository.java
@@ -1,7 +1,9 @@
 package com.piotrgrochowiecki.bookstore.repository;
 
+import com.piotrgrochowiecki.bookstore.model.Author;
 import com.piotrgrochowiecki.bookstore.model.Book;
 import com.piotrgrochowiecki.bookstore.model.Category;
+import com.piotrgrochowiecki.bookstore.model.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -14,5 +16,13 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findBooksByCategory(Category category);
 
     List<Book> findBooksByCategoryId(Long id);
+
+    List<Book> findBooksByAuthor(Author author);
+
+    List<Book> findBooksByPublisher(Publisher publisher);
+
+    List<Book> findBooksByRating(int rating);
+
+    Optional<Book> findFirstBookByCategoryOrderByTitle(Category category);
 
 }
